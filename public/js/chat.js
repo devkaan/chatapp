9bd97fd7208cce94f46d6671e404cbd23b69bc79
kaan <abdikaangrbz@gmail.com>
2021-02-24 21:06:52 +0300

    login and chat page upgraded

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,11 @@
+title = '8chat anlık olarak konuşabileceğin GÜVENLİ bir site.'
 var socket = null;
 var isme = false;
 var asd = 1;
-$(document).ready(function () {
+$(() => {
+  console.log('ready');
   socket = io();
-  $('#comment').keydown(function (e) {
+  $('#comment').on('keydown', (e) => {
     var key = e.keyCode;
     if (key == 13) {
       sendmsg()
@@ -11,14 +13,15 @@ $(document).ready(function () {
     else {
       if ($('#alertbox').css('display') == 'none') {
         $('#alertbox').html('<h6 class="alert alert-warning">You should write a message</h6>');
-        $('#alertbox').show(500).delay(1000).hide(500);
+        $('#alertbox').show(500).delay(1000).hide(500).remove();
       }
     }
   })
-  $("#sendbtn").click(function () {
+  $("#sendbtn").on('click', () => {
     sendmsg()
   });
 
+  isnotifSent = false
   socket.on('refresh feed', ({ text, user }) => {
     var dt = new Date();
     var time = (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ":" + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes());
@@ -26,15 +29,26 @@ $(document).ready(function () {
     var audio = {};
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', '/sounds/bellsound.mp3');
+    audioElement.volume = 0.6
 
     isTabFocused = document.hasFocus();
-    console.log('tab status', isTabFocused);
+    notifVisibility = $('.mute').css('display')
+    console.log(notifVisibility);
     if (isme) {
       text = '<div class="message me"><div class="bubble"><div class="username">' + user + '</div>' + text + '</div><div class="time">' + time + '</div></div>';
     }
     else {
       if (!isTabFocused) {
-        audioElement.play();
+        $('head title', window.parent.document).text('(1) You have a message!');
+        if (notifVisibility != 'none') {
+          if (!isnotifSent) {
+            isnotifSent = true;
+            audioElement.play();
+            setTimeout(() => {
+              isnotifSent = false;
+            }, 7000);
+          }
+        }
       }
       text = '<div class="message"><div class="bubble"><div class="username">' + user + '</div>' + text + '</div><div class="time">' + time + '</div></div>';
     }
@@ -43,7 +57,6 @@ $(document).ready(function () {
 
     // console.log('appended div =>', text);
     lastheight = $(".message").last().height();
-    console.log('last =>', (lastheight * asd * 2));
     $('#message-content').animate({ scrollTop: (lastheight * asd * 2) }, 'fast');
 
     asd++;
@@ -89,4 +102,24 @@ $('#comment').on('keyup', () => {
       $('#sendbtn').hide(200)
     }
   }
+})
+
+$('.volume-btn').on('click', () => {
+  if ($('.mute').css('display') === 'none') {
+    $('.mute').show()
+    $('.unmute').hide()
+  }
+  else {
+    $('.unmute').show()
+    $('.mute').hide()
+  }
+})
+
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState != 'hidden') {
+    $('head title', window.parent.document).text(title);
+  }
+});
+$('html').on('click', () => {
+  $('head title', window.parent.document).text(title);
 })
